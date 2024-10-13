cc3e75190b9384c11ed0c9cc649f305c50d84d5a
Diogoalves2 <diogolsm.barboza39@gmail.com>
2024-10-13 20:15:28 -0300

    New Features: add, delete comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,46 @@
 import styles from'./App.module.css'
 import { Header } from './components/Header';
 import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
 
 import './App.module.css';
 import './global.css';
-import { Sidebar } from './components/Sidebar';
+
+
+//author: { avatar_url: "", name:"", role:""}
+//publishedAt: Date
+//Content: string
+
+const posts =[
+  {
+    id: 1,
+    author:{
+      avatarUrl: 'https://avatars.githubusercontent.com/u/44076060?v=4',
+      name: 'Diogo Alves',
+      role: 'Dev FrontEnd',
+    }, 
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é SkeenCare 🚀' },
+      { type: 'link', content: '👉 jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-10-08 20:00:00'),
+  },
+  {
+    id: 2,
+    author:{
+      avatarUrl: 'https://avatars.githubusercontent.com/u/44076060?v=4',
+      name: 'Janaina Vieira',
+      role: 'Dev FrontEnd',
+    }, 
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: '👉 jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-11-08 20:00:00'),
+  },
+]
 
 export function App() {
   return (
@@ -15,8 +51,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-        <Post />
-        <Post />
+          {posts.map(post => {
+            return (
+            <Post 
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt= {post.publishedAt}
+            />
+            )
+          })}
         </main>
       </div>
       
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,22 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 
 
-export function Comment(){
+export function Comment({content, onDeleteComment}){
+
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment(){
+        onDeleteComment(content);
+    }
+
+    function handleLikeComment(){
+        setLikeCount(likeCount + 1);
+    }
+
+
     return(
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/44076060?v=4" alt="" />
@@ -15,18 +28,18 @@ export function Comment(){
                             <time title="10 de Outubro de 2024" dateTime="2024-10-10 22:00">Cerda de 1h atrás</time>
                         </div>
 
-                        <button title="Deletar Comentário">
+                        <button onClick={handleDeleteComment} title="Deletar Comentário">
                             <Trash size={24} />
                         </button>
                     </header>
 
-                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                    <p>{content}</p>
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp/>
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
